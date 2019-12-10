5e4f6f7f243885887c28356b595f41b09b3dcad5
Andrew Bettke <andrew.bettke@ns8.com>
2019-12-09 23:22:47 -0500

    Setup basic graphql integration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrmModule } from './orm/orm.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     OrmModule,
+    GraphQLModule.forRoot({}),
   ],
 })
 export class AppModule {}
